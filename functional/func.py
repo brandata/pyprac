14134543f3a6007fc48ced3f6f7ea94f8c09368a
Brandon <brandon.haw@gmail.com>
2020-06-30 20:39:06 -0400

    Added functional stuff

--- functional/func.py
+++ functional/func.py
@@ -1,11 +1,25 @@
-# Dummy file for functional programming
+# -*- coding: utf-8 -*-
+"""
+Docstring for the file
+"""
+import collections
 
+Scientist = collections.namedtuple('Scientist', [
+    'name',
+    'field',
+    'born',
+    'nobel',
+])
 
+scientists = (
+    Scientist(name='Ada Lovelace', field='math', born=1815, nobel=False),
+    Scientist(name='Emmy Noether', field='math', born=1882, nobel=False),
+    Scientist(name='Marie Curie', field='physics', born=1867, nobel=True),
+    Scientist(name='Tu Youyou', field='chemistry', born=1930, nobel=True),
+    Scientist(name='Ada Yonath', field='chemistry', born=1939, nobel=True),
+    Scientist(name='Vera Rubin', field='astronomy', born=1928, nobel=False),
+    Scientist(name='Sally Ride', field='physics', born=1951, nobel=False),
+)
 
-
-
-
-
-
-
-
+nobel_only = tuple(filter(lambda x: x.nobel is True, scientists))
+print(nobel_only)
